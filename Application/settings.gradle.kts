4ef88cb9ca46401f588fca96e49e14da34636bbf
karthik-190805 <karthikkamaraj2005@gmail.com>
2026-08-03 20:35:34 +0530

    Add files via upload

---
+++ Application/settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+
+        // ✅ ADD THIS LINE - Required for OSMDroid BonusPack
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "EVApp"
+include(":app")
